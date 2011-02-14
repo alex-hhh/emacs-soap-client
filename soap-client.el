b623687628f2a2747147a8c60db760ec485e7310
Alex Harsanyi <AlexHarsanyi@gmail.com>
2011-02-14 21:04:58 +0800

    added commentary section (soap-load-wsdl-from-url): doc update

--- soap-client.el
+++ soap-client.el
@@ -23,6 +23,18 @@
 
 ;;; Commentary:
 ;; 
+;; To use the SOAP client, you first need to load the WSDL document for the
+;; service you want to access, using `soap-load-wsdl-from-url'.  A WSDL
+;; document describes the available operations of the SOAP service, how their
+;; parameters and responses are encoded.  To invoke operations, you use the
+;; `soap-invoke' method passing it the WSDL, the service name, the operation
+;; you wish to invoke and any required parameters.
+;;
+;; Idealy, the service you want to access will have some documentation about
+;; the operations it supports.  If it does not, you can try using
+;; `soap-inspect' to browse the WSDL document and see the available operations
+;; and their parameters.
+;;
 
 ;;; Code:
 
@@ -707,7 +719,9 @@ traverse an element tree."
 ;;;;; Loading WSDL from XML documents
 
 (defun soap-load-wsdl-from-url (url)
-  "Load a WSDL document from URL and return it."
+  "Load a WSDL document from URL and return it.
+The returned WSDL document needs to be used for `soap-invoke'
+calls."
   (let ((url-request-method "GET")
         (url-package-name "soap-client.el")
         (url-package-version "1.0")
