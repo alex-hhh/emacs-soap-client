61053bd9c66b4dc285ee0d3fff98a876234c7504
Alex Harsanyi <AlexHarsanyi@gmail.com>
2011-02-15 20:45:39 +0800

    soap-inspect.el: fixes suggested by `checkdoc', added Commentary sections

--- soap-inspect.el
+++ soap-inspect.el
@@ -1,8 +1,42 @@
 ;;;; soap-inspect.el -- Interactive inspector for soap WSDL structures
 
+;; Copyright (C) 2010-2011  Alex Harsanyi <AlexHarsanyi@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Alexandru Harsanyi (AlexHarsanyi@gmail.com)
+;; Created: October 2010
+;; Keywords: soap, web-services
+;; Homepage: http://code.google.com/p/emacs-soap-client
+;;
+
+;;; Commentary:
+;; 
+;; This package provides an inspector for a WSDL document loaded with
+;; `soap-load-wsdl' or `soap-load-wsdl-from-url'.  To use it, evaluate:
+;;
+;; (soap-inspect *wsdl*)
+;;
+;; This will pop-up the inspector buffer.  You can click on ports, operations
+;; and types to explore the structure of the wsdl document.
+;;
+
 (require 'soap-client)
 
 
+;;; Code:
+
 ;;; sample-value
 
 (defun soap-sample-value (type)
@@ -19,7 +53,7 @@ will be called."
 
 (defun soap-sample-value-for-basic-type (type)
   "Provide a sample value for TYPE which is a basic type.
-This is a specific function which sould not be called directly,
+This is a specific function which should not be called directly,
 use `soap-sample-value' instead."
   (case (soap-basic-type-kind type)
     (string "a string value")
@@ -33,7 +67,7 @@ use `soap-sample-value' instead."
 Values for sequence types are ALISTS of (slot-name . VALUE) for
 each sequence element.
 
-This is a specific function which sould not be called directly,
+This is a specific function which should not be called directly,
 use `soap-sample-value' instead."
   (let ((sample-value nil))
     (dolist (element (soap-sequence-type-elements type))
@@ -41,7 +75,7 @@ use `soap-sample-value' instead."
                   (soap-sample-value (soap-sequence-element-type element)))
             sample-value))
     (when (soap-sequence-type-parent type)
-      (setq sample-value 
+      (setq sample-value
             (append (soap-sample-value (soap-sequence-type-parent type))
                     sample-value)))
     sample-value))
@@ -51,7 +85,7 @@ use `soap-sample-value' instead."
 Values for array types are LISP vectors of values which are
 array's element type.
 
-This is a specific function which sould not be called directly,
+This is a specific function which should not be called directly,
 use `soap-sample-value' instead."
   (let* ((element-type (soap-array-type-element-type type))
          (sample1 (soap-sample-value element-type))
@@ -61,8 +95,8 @@ use `soap-sample-value' instead."
     (vector sample1 sample2 '&etc)))
 
 (defun soap-sample-value-for-message (message)
-  "Provide a sample value for a WSDL message.
-This is a specific function which sould not be called directly,
+  "Provide a sample value for a WSDL MESSAGE.
+This is a specific function which should not be called directly,
 use `soap-sample-value' instead."
   ;; NOTE: parameter order is not considered.
   (let (sample-value)
@@ -81,7 +115,7 @@ use `soap-sample-value' instead."
        'soap-sample-value-for-seqence-type)
 
   (put (aref (make-soap-array-type) 0) 'soap-sample-value
-       'soap-sample-value-for-array-type)      
+       'soap-sample-value-for-array-type)
 
   (put (aref (make-soap-message) 0) 'soap-sample-value
        'soap-sample-value-for-message) )
@@ -104,11 +138,11 @@ Used to implement the BACK button.")
 (defun soap-inspect (element)
   "Inspect a SOAP ELEMENT in the *soap-inspect* buffer.
 The buffer is populated with information about ELEMENT with links
-to its subelements.  If ELEMENT is the WSDL document itself, the
+to its sub elements.  If ELEMENT is the WSDL document itself, the
 entire WSDL can be inspected."
   (let ((inspect (get (aref element 0) 'soap-inspect)))
     (unless inspect
-      (error "soap-inspect: no inspector for element"))
+      (error "Soap-inspect: no inspector for element"))
 
     (with-current-buffer (get-buffer-create "*soap-inspect*")
       (setq buffer-read-only t)
@@ -154,23 +188,24 @@ entire WSDL can be inspected."
     'skip t)
 
 (defun soap-insert-describe-button (element)
-  "Insert a button with the ELEMENT's name which will inspect
-ELEMENT when pressed."
+  "Insert a button to inspect ELEMENT when pressed."
   (insert-text-button
    (soap-element-fq-name element)
    'type 'soap-client-describe-link
    'item element))
 
 (defun soap-inspect-basic-type (basic-type)
+  "Insert information about BASIC-TYPE into the current buffer."
   (insert "Basic type: " (soap-element-fq-name basic-type))
   (insert "\nSample value\n")
   (pp (soap-sample-value basic-type) (current-buffer)))
 
 (defun soap-inspect-sequence-type (sequence)
+  "Insert information about SEQUENCE into the current buffer."
   (insert "Sequence type: " (soap-element-fq-name sequence) "\n")
   (when (soap-sequence-type-parent sequence)
     (insert "Parent: ")
-    (soap-insert-describe-button 
+    (soap-insert-describe-button
      (soap-sequence-type-parent sequence))
     (insert "\n"))
   (insert "Elements: \n")
@@ -188,23 +223,26 @@ ELEMENT when pressed."
   (pp (soap-sample-value sequence) (current-buffer)))
 
 (defun soap-inspect-array-type (array)
+  "Insert information about the ARRAY into the current buffer."
   (insert "Array name: " (soap-element-fq-name array) "\n")
   (insert "Element type: ")
-  (soap-insert-describe-button 
+  (soap-insert-describe-button
    (soap-array-type-element-type array))
   (insert "\nSample value:\n")
   (pp (soap-sample-value array) (current-buffer)))
 
 (defun soap-inspect-message (message)
+  "Insert information about MESSAGE into the current buffer."
   (insert "Message name: " (soap-element-fq-name message) "\n")
   (insert "Parts:\n")
   (dolist (part (soap-message-parts message))
-    (insert "\t" (symbol-name (car part)) 
+    (insert "\t" (symbol-name (car part))
             " type: ")
-    (soap-insert-describe-button (cdr part)) 
+    (soap-insert-describe-button (cdr part))
     (insert "\n")))
 
 (defun soap-inspect-operation (operation)
+  "Insert information about OPERATION into the current buffer."
   (insert "Operation name: " (soap-element-fq-name operation) "\n")
   (let ((input (soap-operation-input operation)))
     (insert "\tInput: " (symbol-name (car input)) " (" )
@@ -222,15 +260,17 @@ ELEMENT when pressed."
       (pp sample-invocation (current-buffer)))))
 
 (defun soap-inspect-port-type (port-type)
+  "Insert information about PORT-TYPE into the current buffer."
   (insert "Port-type name: " (soap-element-fq-name port-type) "\n")
   (insert "Operations:\n")
-  (loop for o being the hash-values of 
+  (loop for o being the hash-values of
        (soap-namespace-elements (soap-port-type-operations port-type))
        do (progn
             (insert "\t")
             (soap-insert-describe-button (car o)))))
 
 (defun soap-inspect-binding (binding)
+  "Insert information about BINDING into the current buffer."
   (insert "Binding: " (soap-element-fq-name binding) "\n")
   (insert "\n")
   (insert "Bound operations:\n")
@@ -261,12 +301,14 @@ ELEMENT when pressed."
         (insert "\n")))))
 
 (defun soap-inspect-port (port)
+  "Insert information about PORT into the current buffer."
   (insert "Port name:   " (soap-element-name port) "\n"
           "Service URL: " (soap-port-service-url port) "\n"
           "Binding:     ")
   (soap-insert-describe-button (soap-port-binding port)))
 
 (defun soap-inspect-wsdl (wsdl)
+  "Insert information about WSDL into the current buffer."
   (insert "WSDL Origin: " (soap-wsdl-origin wsdl) "\n")
   (insert "Ports:")
   (dolist (p (soap-wsdl-ports wsdl))
@@ -275,10 +317,10 @@ ELEMENT when pressed."
     (soap-inspect-port p))
   (insert "\n--------------------\nNamespace alias table:\n")
   (dolist (a (soap-wsdl-alias-table wsdl))
-    (insert "\t" (car a) " => " (cdr a) "\n"))
-)
+    (insert "\t" (car a) " => " (cdr a) "\n")))
 
 (progn
+  ;; Install the soap-inspect methods for our types
 
   (put (aref (make-soap-basic-type) 0) 'soap-inspect
        'soap-inspect-basic-type)
@@ -304,8 +346,7 @@ ELEMENT when pressed."
        'soap-inspect-port)
 
   (put (aref (make-soap-wsdl) 0) 'soap-inspect
-       'soap-inspect-wsdl)
-
-  )
+       'soap-inspect-wsdl))
 
 (provide 'soap-inspect)
+;;; soap-inspect.el ends here
